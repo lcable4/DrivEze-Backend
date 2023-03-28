ead385dce0bb34df659dfe775e70fda11ee7295a
victor <vvazquez307@gmail.com>
2023-03-28 10:46:16 -0500

    added the client

---
+++ db/seed.js
@@ -0,0 +1,46 @@
+const {
+  client,
+  getAllUsers,
+  createUser,
+  updateUser,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
+  createTags,
+  addTagsToPost,
+  createPostTag,
+  getPostsByTagName,
+  getAllTags,
+  getUserByUsername,
+  getPostById,
+} = require("./index");
+
+async function dropTables() {
+  try {
+    // console.log("Starting to drop tables...");
+    // await client.query(`
+    // DROP TABLE IF EXISTS
+    // DROP TABLE IF EXISTS
+    // DROP TABLE IF EXISTS
+    // DROP TABLE IF EXISTS `
+    // );
+    // console.log("Finished dropping tables!");
+  } catch (error) {
+    console.log("Error when dropping tables!");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+    await client.query(`
+    `);
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.log("Error when building tables!");
+    throw error;
+  }
+}
