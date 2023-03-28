34e19436b058852bf3fe807c5b4ae2bb4f1cd43c
victor <vvazquez307@gmail.com>
2023-03-28 12:30:36 -0500

    added all tables

--- db/seed.js
+++ db/seed.js
@@ -20,6 +20,45 @@ async function createTables() {
   try {
     console.log("Starting to build tables...");
     await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      location VARCHAR(255) NOT NULL,
+      active BOOLEAN DEFAULT TRUE,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+      );
+
+      CREATE TABLE cars(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        price INTEGER NOT NULL,
+        "hubLocation" VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT TRUE,
+      );
+
+      CREATE TABLE tags(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL
+      );
+
+      CREATE TABLE car_tags(
+        id SERIAL PRIMARY KEY,
+        "carId" INTEGER REFERENCES cars(id),
+        "tagId" INTEGER REFERENCES tags(id),
+        UNIQUE("carId","tagId")
+      );
+
+      CREATE TABLE hubs(
+        id SERIAL PRIMARY KEY,
+        location VARCHAR(255) UNIQUE NOT NULL,
+      );
+
+      CREATE TABLE inventory(
+        id SERIAL PRIMARY KEY,
+        "hubId" INTEGER REFERENCES hubs(id),
+        "carId" INTEGER REFERENCES cars(id)
+      );
     `);
     console.log("Finished building tables!");
   } catch (error) {
