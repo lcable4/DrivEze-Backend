a36acfa54de6a6bf62739c57bcbaf5ffaad13639
victor <vvazquez307@gmail.com>
2023-03-31 09:21:25 -0500

    testDB

--- db/inventory.js
+++ db/inventory.js
@@ -1,18 +1,27 @@
 const client = require("./index");
 
+async function addCarToHubInventory(carId, hubId) {
+  try {
+    await client.connect();
 
-async function addCarToHubInventory(carId, hubId)
-{
+    const {
+      rows: [inventory],
+    } = await client.query(
+      `
+            INSERT INTO inventory("carId", "hubId")
+            VALUES ($1, $2)
+            ON CONFLICT("carId, "hubId") DO NOTHING;
+        `,
+      [carId, hubId]
+    );
 
+    await client.release();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
-async function removeCarFromHubInventory(carId, hubId)
-{
+async function removeCarFromHubInventory(carId, hubId) {}
 
-}
-
-async function getInventoryByHubId(hubId)
-{
-
-}
-
+async function getInventoryByHubId(hubId) {}
