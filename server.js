86ee35567b2dd34a558c0047431918f51b3ed8d6
Logan <logancable@ymail.com>
2023-04-04 09:32:42 -0700

    added cors

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const PORT = 3000;
 const express = require("express");
 const server = express();
-
-
-
 const morgan = require("morgan");
+const cors = require("cors");
+
 server.use(morgan("dev"));
 server.use(express.json());
+server.use(cors());
 
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
@@ -16,4 +16,3 @@ server.use("/api", apiRouter);
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
 });
-
