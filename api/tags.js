5706295c8853f5473569a9e52fc50729aa8f7759
Logan <logancable@ymail.com>
2023-04-03 19:32:40 -0700

    finished api/tags functions and testing

--- api/tags.js
+++ api/tags.js
@@ -10,6 +10,7 @@ const {
 } = require("../db/tags");
 const tagsRouter = express.Router();
 
+//Post a new tag
 //POST /api/tags/
 tagsRouter.post("/", async (req, res, next) => {
   try {
@@ -21,6 +22,7 @@ tagsRouter.post("/", async (req, res, next) => {
   }
 });
 
+//Get all tags
 //GET /api/tags/
 tagsRouter.get("/", async (req, res, next) => {
   try {
@@ -28,6 +30,65 @@ tagsRouter.get("/", async (req, res, next) => {
     res.send(tags);
   } catch (error) {
     next(error);
+  }
+});
+
+//Gets one tag by ID
+//GET /api/tags/:id
+tagsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const tags = await getTagById(id);
+    res.send(tags);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Updates one tag by ID
+//PATCH /api/tags/:id
+tagsRouter.patch("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const updatedTag = await updateTag({ tagId: id, name });
+    res.send(updatedTag);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Deactivates a tag matching the ID
+//PATCH /api/tags/deactivateTag/:id
+tagsRouter.patch("/deactivateTag/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const rowCount = await deactivateTag(id);
+    if (rowCount === 1) {
+      res.status(200).json({ message: `Tag ${id} has been deactivated.` });
+    } else {
+      res.status(404).json({ message: `Tag ${id} not found.` });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
+
+//Deletes a tag match the ID
+//DELETE /api/tags/deleteTag/:id
+tagsRouter.delete("/deleteTag/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedTag = await deleteTag(id);
+    if (deletedTag) {
+      res.status(200).json({ message: `Tag ${id} has been deleted.` });
+    } else {
+      res.status(404).json({ message: `Tag ${id} was not found` });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error." });
   }
 });
 
--- db/tags.js
+++ db/tags.js
@@ -1,176 +1,136 @@
-const client = require('./index');
+const client = require("./index");
 
-async function createTag(name)
-//creates a tag with the given name
-{
-    try
-    {
-        await client.connect();
+async function createTag(name) {
+  //creates a tag with the given name
+  try {
+    await client.connect();
 
-        const
-         {
-            rows
-        }
-        = await client.query(`
+    const { rows } = await client.query(
+      `
         SELECT * FROM tags
         WHERE name = ($1);
         `,
-        [name]
-        );
-        if(rows.length > 0)
-        {
-            console.log(`Tag with name "${name}" already exists`);
-            return;
-        } 
-        const
-        {
-            rows:tag
-        }
-        = await client.query(`
+      [name]
+    );
+    if (rows.length > 0) {
+      console.log(`Tag with name "${name}" already exists`);
+      return;
+    }
+    const { rows: tag } = await client.query(
+      `
             INSERT INTO tags(name)
             VALUES ($1)
             RETURNING *
             `,
-            [name]
-        );
-        await client.release();
-        return tag;
-        //returns the newly created tag
-    }
-    catch(e)
-    {
-        console.error(e);
-    }
+      [name]
+    );
+    await client.release();
+    return tag;
+    //returns the newly created tag
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-async function getAllTags()
-//gets all tags from the database
-{
-    try
-    {
-        client.connect();
+async function getAllTags() {
+  //gets all tags from the database
+  try {
+    client.connect();
 
-        const 
-        {
-            rows:tags
-        }
-        = await client.query(`
+    const { rows: tags } = await client.query(`
             SELECT * FROM tags
         `);
-        
-        await client.release();
-        return tags;
-        //returns an array of all tags
 
-    }
-    catch(e)
-    {
-        console.error(e);
-    }
+    await client.release();
+    return tags;
+    //returns an array of all tags
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-async function getTagById(tagId)
-//gets all the info from a specific tag based on the given id
-{
-    try
-    {
-        client.connect();
+async function getTagById(tagId) {
+  //gets all the info from a specific tag based on the given id
+  try {
+    client.connect();
 
-        const 
-        {
-          rows:[tag],
-        }
-        // selects all information from tags where the id is equal to $1
-        = await client.query(` 
+    const {
+      rows: [tag],
+    } =
+      // selects all information from tags where the id is equal to $1
+      await client.query(
+        ` 
           SELECT *
           FROM tags
           WHERE id = $1;
         `,
         [tagId]
-        );
+      );
 
-        await client.release();
-        return tag;  
-        // returns all the tag info from a specific tag based on the id its given
-    }
-    catch(e)
-    {
-        console.error(e);
-    }
+    await client.release();
+    return tag;
+    // returns all the tag info from a specific tag based on the id its given
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-async function updateTag({tagId, name})
 //updates a tag based on the given tagId and fields
-{
-  try
-  {
+async function updateTag({ tagId, name }) {
+  try {
+    await client.connect();
 
-      await client.connect();
-
-      const 
-      {
-          rows:[tag],
-      }
-      = await client.query(`
+    const {
+      rows: [tag],
+    } = await client.query(
+      `
           UPDATE tags
           SET name = $1
           WHERE id = $2
           RETURNING *;
       `,
       [name, tagId]
-      );
+    );
 
-      await client.release();
-      return tag;
-      //returns the updated tag
-  }
-  catch(e)
-  {
-      console.error(e);
+    await client.release();
+    return tag;
+    //returns the updated tag
+  } catch (e) {
+    console.error(e);
   }
 }
 
-async function deactivateTag(tagId)
-//switches a tag from active to inactive so that it won't display 
-{
-  try 
-  {
+async function deactivateTag(tagId) {
+  //switches a tag from active to inactive so that it won't display
+  try {
     await client.connect();
 
-    const 
-    {
-      rowCount
-
-    } 
-    = await client.query(`
+    const { rowCount } = await client.query(
+      `
       UPDATE tags
       SET active = false
       WHERE id=$1;
       `,
       [tagId]
-      );
+    );
 
     await client.release();
     return rowCount;
     //returns the deactivated tag back
-   } 
-  catch (e) 
-  {
+  } catch (e) {
     console.error(e);
   }
 }
 
 async function deleteTag(tagId) {
-//deletes a tag based on the tagId given
-  try 
-  {
+  //deletes a tag based on the tagId given
+  try {
     await client.connect();
 
-    const 
-    {
+    const {
       rows: [tag],
-    } 
-    = await client.query(`
+    } = await client.query(
+      `
          DELETE FROM tags
          WHERE id=$1
          RETURNING *;
@@ -182,19 +142,16 @@ async function deleteTag(tagId) {
 
     return tag;
     //returns all tags minus the one deleted
-  } 
-  catch (e) 
-  {
+  } catch (e) {
     console.error(e);
   }
 }
 
 module.exports = {
-    createTag,
-    getAllTags,
-    getTagById,
-    updateTag,
-    deactivateTag,
-    deleteTag,
-    
-}
+  createTag,
+  getAllTags,
+  getTagById,
+  updateTag,
+  deactivateTag,
+  deleteTag,
+};
