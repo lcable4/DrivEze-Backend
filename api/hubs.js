85eb202c8592564910fd9cf74b880edb197691f0
TheHighnote <ryanhighnote7@gmail.com>
2023-04-04 09:50:53 -0500

    changed formatting to be easier to read

--- api/hubs.js
+++ api/hubs.js
@@ -1,90 +1,90 @@
 const express = require("express");
-const router = express.Router();
-const {getAllHubs, createHub, getHubById, updateHub, deleteHub}  = require("../db/hubs");
+const hubsRouter = express.Router();
+const {
+  getAllHubs,
+  createHub,
+  getHubById,
+  updateHub,
+  deleteHub,
+} = require("../db/hubs");
 
+hubsRouter.get("/", async (req, res, next) => {
+  try {
+    const allHubs = await getAllHubs();
+    const hubs = allHubs.filter((hub) => {
+      return hub;
+    });
 
-router.get("/", async (req, res, next) => {
-    try {
-      const allHubs = await getAllHubs();
-      const hubs = allHubs.filter((hub) => {
-        return hub;
-      });
-  
-      res.send(hubs);
-    } catch ({ location }) {
-      next({ location });
+    res.send(hubs);
+  } catch ({ location }) {
+    next({ location });
+  }
+});
+
+hubsRouter.post("/", async (req, res, next) => {
+  const { location = "" } = req.body;
+
+  const hubData = {
+    location,
+  };
+
+  try {
+    const hub = await createHub(hubData);
+    if (hub) {
+      res.send(hub);
+    } else {
+      next({ name: "hubCreationError", message: "Error creating hub." });
     }
-  });
+  } catch ({ name, message }) {
+    next({
+      name,
+      message: `An activity with name ${hubData.hub} already exists`,
+    });
+  }
+});
 
-  router.post("/", async (req, res, next) => {
-    const { location = "" } = req.body;
-  
-    const hubData = {
-      location
-    };
-  
-    try {
-      const hub = await createHub(hubData);
-      if (hub) {
-        res.send(hub);
-      } else {
-        next({ name: "hubCreationError", message: "Error creating hub." });
-      }
-    } catch ({ name, message }) {
+hubsRouter.patch("/:hubId", async (req, res, next) => {
+  try {
+    const { hubId } = req.params;
+    const getHubId = await getHubById(hubId);
+    if (!getHubId) {
       next({
-        name,
-        message: `An activity with name ${hubData.hub} already exists`,
+        name: "not found",
+        message: `Hub ${hubId} not found`,
       });
-    }
-  });
-
-  router.patch("/:hubId", async (req, res, next) => {
-    try {
-      const { hubId } = req.params;
-      const getHubId = await getHubById(hubId);
-      if (!getHubId) {
-        next({
-          name: "not found",
-          message: `Hub ${hubId} not found`,
+    } else {
+      const { location } = req.body;
+      try {
+        const updatedHub = await updateHub({
+          id: hubId,
+          location,
         });
-      } else {
-        const { location } = req.body;
-        try {
-          const updatedHub = await updateHub({
-            id: hubId,
-            location
-          });
-          res.send(updatedHub);
-        } catch (error) {
-          next({
-            name: "",
-            message: `An hub with location ${location} already exists`,
-          });
-          console.log(error);
-        }
+        res.send(updatedHub);
+      } catch (error) {
+        next({
+          name: "",
+          message: `An hub with location ${location} already exists`,
+        });
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-  router.delete("/:hubId", async (req, res, next) => {
-    try{
-        const {hubId} = req.params;
-        const hub = await getHubById(hubId);
-        console.log(hub, "hubId log")
-        if (!hub) {
-            next({
-              name: "Error",
-              message: "Hub does not exist!",
-             });
-          } else {
-            const deleteHub = await deleteHub(hubId);
-            res.send(hub);
-          }
-        const deleteHub = await deleteHub(hubId);
-        res.send(hub);
-        } catch ({ name, message }) {
-          next({ name, message });
-        }
-})
+hubsRouter.delete("/deleteHub/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedHub = await deleteHub(id);
+    if (deletedHub) {
+      res.status(200).json({ message: `Hub ${id} was deleted.` });
+    } else {
+      res.status(404).json({ message: `Hub ${id} could not be found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error." });
+    next(error);
+  }
+});
+
