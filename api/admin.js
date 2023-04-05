be54048a58c72b2dce63255b394d058bf6781ba4
UpcomingWhale <123348874+JavaScriptCodingGuru@users.noreply.github.com>
2023-04-05 00:35:44 -0400

    added admin
    added route /api/admin
    added db file admin.js
    -createAdmin()
    -getAdmin() || Login
    added api file admin.js
    -post ".../admin/login"

---
+++ api/admin.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { getAdmin } = require("../db/admin");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+const adminRouter = express.Router();
+
+adminRouter.post("/login", async(req, res, next)=>
+{
+    const {username, password} = req.body;
+    console.log(req.admin);
+    if (!username || !password) {
+        next({
+          name: "MissingCredentialsError",
+          message: "Please supply both a username and password",
+        });
+      }
+
+      try
+      {
+        const user = await getAdmin({username, password});
+        if(user)
+        {
+            const token = jwt.sign(
+                {
+                    id: user.id,
+                    username,
+                    isAdmin: user.isAdmin
+                },
+                JWT_SECRET
+                );
+            res.send({message:"You are logged in to admin", token});
+        }
+        else
+        {
+            next({name:"IncorrectCredentialsError", message:"Username or Password is incorrect",})
+        }
+      }
+      catch(e)
+      {
+        throw e;
+      }
+})
+
+module.exports = adminRouter;
